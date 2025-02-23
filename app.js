a947df12b65c4f36b7790752a4a185785d67b5d0
mariovergara7761 <mario.vergara.jara@gmail.com>
2025-02-23 20:56:05 -0300

    actualizar lista

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 /*Funcionalidades:
-commit "agregarnombre" Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+commit "agregar nombre" Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar". Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
 
-Validar entrada: Si el campo de texto está vacío, el programa mostrará una alerta pidiendo un nombre válido.
-
-Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
+comit "actualizar lista"Visualizar la lista: Los nombres ingresados aparecerán en una lista debajo del campo de entrada.
 
 Sorteo aleatorio: Al hacer clic en el botón "Sortear Amigo", se seleccionará aleatoriamente un nombre de la lista y se mostrará en la página.*/
 
@@ -17,16 +15,22 @@ function agregarAmigo() {
     }
     
     amigos.push(ingresarNombre);
-    console.log(amigos);
     limpiarCaja();
+    actualizarLista();
 
 }
 
 function limpiarCaja() {
     document.querySelector("#amigo").value = '';
-    
+    return;
 }
 
-
-
-
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
